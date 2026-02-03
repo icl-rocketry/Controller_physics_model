6d3b16438772beaf633aec53b8d4f008644da8b9
Leo Sutherby <leo.sutherby24@imperial.ac.uk>
2026-02-03 18:32:20 +0000

    loading aero tables

--- LoadAeroTables.m
+++ LoadAeroTables.m
@@ -1,5 +1,6 @@
-function [CA_xpitch_spline, CN_xpitch_spline, CMy_xpitch_spline, CA_static_spline, CN_static_spline, CMy_static_spline] = LoadAeroTables()
-
+function [CA_xpitch_spline, CN_xpitch_spline, CMy_xpitch_spline, CA_static_spline, CN_static_spline, CMy_static_spline, CLfit, CDfit] = LoadAeroTables()
+    
+    %ROCKET
     static_table_raw = readtable("B0000.xlsx");
     dynamic_table_raw = readtable("xpitch_B0000.xlsx");
     %fin_table_raw = readtable("fin_table");
@@ -22,7 +23,6 @@ function [CA_xpitch_spline, CN_xpitch_spline, CMy_xpitch_spline, CA_static_splin
     CN_static = static_table_raw(:,8);
     CMy_static = static_table_raw(:,12);
 
-
     %xpitch fits
     x = [M_xpitch, alpha_xpitch];
     CA_xpitch_spline = tpaps(x', CA_xpitch', 0.8);
@@ -38,5 +38,25 @@ function [CA_xpitch_spline, CN_xpitch_spline, CMy_xpitch_spline, CA_static_splin
     %to evaluate a particular value: fnval(CA_xpitch_spline, query_point),
     %where query_point = [M, alpha]
     %to plot: fnplt(CA_xpitch_spline)
+
+    %GRIDFINS
+    CL_alpha_table = readmatrix("honeycomb-clalpha.xlsx");
+    CD_alpha_table = readmatrix("honeycomb-cdalpha.xlsx");
+    
+    %get rid of random values at the end
+    CL_alpha_table = CL_alpha_table(2:90, :);
+
+    %1D spline fits
+    %to calcuate value at a point: Clval = ppval(Clfit, alpha);
+    %do not exceed +- 50 degrees aoa
+
+    Clalphaextended = [-flip(CL_alpha_table(:,1)); CL_alpha_table(:,1)];
+    Clextended = [flip(CL_alpha_table(:,2)); CL_alpha_table(:,2)];
+
+    Cdalphaextended = [-flip(CD_alpha_table(:,1)); CD_alpha_table(:,1)];
+    Cdextended = [flip(CD_alpha_table(:,2)); CD_alpha_table(:,2)];
+
+    CLfit = spline(Clalphaextended, Clextended);
+    CDfit = spline(Cdalphaextended, Cdextended);
 
 end
