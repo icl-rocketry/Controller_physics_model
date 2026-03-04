6577b531b2b469847dbe775cb99ffa989ab5ea74
youngers2006 <sy2224@ic.ac.uk>
2026-03-04 13:46:37 +0000

    added GF state handling

--- dynamics_fn.m
+++ dynamics_fn.m
@@ -1,6 +1,5 @@
 function [x_dot, x_add_dot] = dynamics_fn(t, x, x_add, u, rocket_config)
 % calculates force and torque on the body to get the state derivative of the system
-    
     % Obtain Cg and inertia tensor
     [r_cg, J] = Get_inertia_and_mass(x(14), ...
         x_add(1), x_add(2), config.r_dry, config.m_dry, config.R_ox, ...
@@ -16,22 +15,35 @@ function [x_dot, x_add_dot] = dynamics_fn(t, x, x_add, u, rocket_config)
     R_BI = quat2rotm(q);
     v_body = x(4:6) * transpose(R_BI);
 
-    % Calculate aerodynamic force and torque in body axes
-    u_grid_fins = u(1:3); % note: for now assume infinite precision
+    x_add_dot = zeros(size(x_add));
 
-    % add actuator model here
+    % grid fin actuator model
+    for i = 1:3
+        actuator_state = x_add(2 + i);
+        x_add_dot_GF = GF_actuator_dynamic_fn(actuator_state, ...
+            u(i), rocket_config);
+        x_add_dot(2 + i) = x_add_dot_GF;
+    end
+
+    % grid fin control inputs
+    u_grid_fins = u(1:3);
 
     % calculate alpha and beta (z and y)
     alpha = atan2(v_body(3), -v_body(1));
     beta = atan2(v_body(2), -v_body(1));
-    [F_aero_body, tau_aero_body] = Aerodynamic_forces(x, u_grid_fins, alpha, beta, x_cg, rocket_config);
+
+    % Calculate aerodynamic force and torque in body axes
+    [F_aero_body, tau_aero_body] = Aerodynamic_forces(x, ...
+        u_grid_fins, alpha, beta, x_cg, rocket_config);
     
     % Calculate force and torque from engine in body axes
     u_engine = u(4:6);
     [F_thrust_body, tau_thrust_body, Isp] = Engine_Model(); % need to finish
 
     % Caculates state derivative with forces and torques
-    [x_dot, x_add_dot] = Get_state_derivative(t, ...
+    [x_dot, x_add_dot_fuel] = Get_state_derivative(t, ...
         x, R_BI, J, F_thrust_body, tau_thrust_body, F_aero_body, ...
         tau_aero_body, Isp, rocket_config.OF, rocket_config.g0);
+
+    x_add_dot(1:2) = x_add_dot_fuel(:); 
 end
