b4c42b5b82e372d71aa10ca16e4ad70572f0434f
youngers2006 <sy2224@ic.ac.uk>
2026-05-21 20:55:42 +0100

    Finished rocket dynamics simulink, remaining tasks are finishing engine model functions and adding prop slosh model (not entirely sure about this yet)

--- Engine_relight_model/relight_thrust_model.m
+++ Engine_relight_model/relight_thrust_model.m
@@ -1,3 +1,4 @@
-function [] = relight_thrust_model()
+function [T] = relight_thrust_model(t)
 % calculates the output thrust of the engine during relight phase
+    % For now just a linear fit in time
 end
